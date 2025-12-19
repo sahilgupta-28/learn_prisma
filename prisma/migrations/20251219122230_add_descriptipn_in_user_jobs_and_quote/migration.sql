21374640147a5e60680cfc6f5ff4bf5d8d7521c3
sahil gupta <sahil.gupta@devs.matrixmarketers.com>
2025-12-19 17:53:20 +0530

    update migrations

---
+++ prisma/migrations/20251219122230_add_descriptipn_in_user_jobs_and_quote/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Jobs" ADD COLUMN     "description" TEXT;
+
+-- AlterTable
+ALTER TABLE "Quote" ADD COLUMN     "description" TEXT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "description" TEXT;
